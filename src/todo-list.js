780ef7c3a18cc9bce8adfd31e7db3e49028dc1f6
Kate Schumacher <kate.schumacher@gmail.com>
2024-12-16 21:40:43 -0600

    Write basic todo list class

--- src/todo-list.js
+++ src/todo-list.js
@@ -1,5 +1,18 @@
 //Contains multiple todo items
 
 class TodoList {
-    
+    constructor () {
+        let list = [];
+    }
+    addItemToList (item) {
+        list.push(item);
+    }
+    removeItemFromList (item) {
+        let oldIndex = item.listIndex;
+        // Shifts the index number of each following item
+        list.splice(oldIndex, 1);
+        for (let i = oldIndex; i < list.length; i++) {
+            list[i].listIndex = i;
+        };
+    }
 }
