5f362ccdcec738f1f1468c9ae1854f211b2a360e
Kate Schumacher <kate.schumacher@gmail.com>
2024-12-17 11:42:16 -0600

    Write rudimentary DOM function to draw modal

--- src/index.js
+++ src/index.js
@@ -1,27 +1,10 @@
 import "./styles.css";
 import { TodoList } from "./todo-list";
 import { TodoItem, todoItemController } from "./todo-item";
+import { drawModal, killModal } from "./newItemPage";
 
-const testList = new TodoList();
-console.log(testList);
-const testTask = todoItemController.generateTodoItem("Test this shit", "It's going to be kind of difficult, but that's OK.", "12/22/2024", 0, 0);
-testList.addItemToList(testTask);
-console.log(testList);
-console.log(testTask.description);
-const testTask1 = todoItemController.generateTodoItem("Wtf", "It's going to be kind of difficult, but that's OK.", "12/22/2024", 0, testList.getListLength());
-testList.addItemToList(testTask1);
-
-const testTask2 = todoItemController.generateTodoItem("Aaa", "It's going to be kind of difficult, but that's OK.", "12/22/2024", 0, testList.getListLength());
-testList.addItemToList(testTask2);
-
-
-const testTask3 = todoItemController.generateTodoItem("Lol", "It's going to be kind of difficult, but that's OK.", "12/22/2024", 0, testList.getListLength());
-testList.addItemToList(testTask3);
-console.log(testList.getList());
-console.log(testList.getListLength());
-console.log(testList.removeItemFromList(testTask1));
-console.log(testList.getList());
-
+drawModal();
+killModal();
 
 // STEPS:
 // 1.)Write the logic that stores 
