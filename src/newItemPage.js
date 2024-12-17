287a4e5ba8cb05b5d48ff201847b9e601e2c27fd
Kate Schumacher <kate.schumacher@gmail.com>
2024-12-17 12:03:21 -0600

    Add skeleton form to add task modal

--- src/newItemPage.js
+++ src/newItemPage.js
@@ -1,19 +1,14 @@
-function drawModal () {
-    const container = document.querySelector(".modal");
+// function drawModal () {
+//     const container = document.querySelector(".modal");
 
-    const modalBackground = document.createElement("div");
-    modalBackground.classList.add("modal-background");
-    container.appendChild(modalBackground);
+//     const modalBackground = document.createElement("div");
+//     modalBackground.classList.add("modal-background");
+//     container.appendChild(modalBackground);
 
-    const modalPopup = document.createElement("div");
-    modalPopup.textContent = "Testing!";
-    modalPopup.classList.add("modal-content");
-    modalBackground.appendChild(modalPopup);
-}
+//     const modalPopup = document.createElement("div");
+//     modalPopup.textContent = "Testing!";
+//     modalPopup.classList.add("modal-content");
+//     modalBackground.appendChild(modalPopup);
+// }
 
-function killModal () {
-    const container = document.querySelector(".modal");
-    container.innerHTML = "";
-}
-
-export{ drawModal, killModal };
+// export{ drawModal };
