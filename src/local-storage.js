42fcaca151466787d75c9e53122c2d71ef04fcc5
Kate Schumacher <kate.schumacher@gmail.com>
2025-01-02 10:56:01 -0600

    Write function to store all data as JSON key pairs

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,14 +1,30 @@
 function createStorage() {
     return {
         saveData(rootList) {
-            console.log("Attempting to save data");
-            console.log(rootList);
-            for (const [key, value] of Object.entries(rootList)) {
-                console.log(`${key}: ${value}`);
-                if (key == "list") {
-                    for (let i = 0; i < rootList.list.length; i++) {
-                        for (const [key, value] of Object.entries(rootList.list[i])) {
-                            console.log(`${key}: ${value}`);
+            stringifyList(rootList);
+            
+            function stringifyList(list) {
+                // console.log("Stringifying a list.");
+                for (const [key, value] of Object.entries(list)) {
+                    console.log(`${key}: ${value}`);
+                    JSON.stringify({key: value});
+                    console.log("Key value pair as JSON: " + JSON.stringify(key, value));
+                    window.localStorage.setItem(key, value);
+                    if (key === "list") {
+                        // console.log("Above is a list. Printing list");
+                        for (let i = 0; i < list.list.length; i++) {
+                            // console.log(list.list[i]);
+                            if (list === rootList) {
+                                // console.log("List inputted is rootList. Looping again.");
+                                stringifyList(list.list[i])
+                            } else {
+                                for (const [key, value] of Object.entries(list.list[i])) {
+                                    console.log(`${key}: ${value}`);
+                                    JSON.stringify({key: value});
+                                    console.log("Key value pair as JSON: " + JSON.stringify(key, value));
+                                    window.localStorage.setItem(key, value);
+                                }
+                            }
                         }
                     }
                 }
