5ec0b99ac9a0bc5245c0cf88877396672579b261
Kate Schumacher <kate.schumacher@gmail.com>
2025-01-03 13:03:26 -0600

    Remove final console.logs

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,21 +1,14 @@
 function createStorage() {
     return {
         saveData(rootList) {
-            console.log("Saving data.");
             window.localStorage.clear();
             window.localStorage.setItem("rootList", JSON.stringify(rootList));
-            console.log("Stringified rootList:");
-            console.log(JSON.stringify(rootList));
         },
         loadData() {
-            console.log("Retrieving data.");
             if (window.localStorage.getItem("rootList") === null) {
-                console.log("No saved rootList found. Returning false.")
                 return false;
             }
             let rootList = window.localStorage.getItem("rootList");
-            console.log("Rootlist found. Retrieved rootlist as JSON:");
-            console.log(rootList);
             return JSON.parse(rootList);
         }
     }
